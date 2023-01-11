4f4ff01b3320f66ce124e61c07e5ea44ff5461f0
AkaCyberRat <andrejchuk01@gmail.com>
2023-01-12 00:06:24 +0300

    Refused transaction manager in favor syncronization

--- internal/repository/transaction_manager.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-type TransactionManager interface {
-	Transaction(txFunc InTransaction) (out interface{}, err error)
-
-	GetPeerRepository() PeerRepository
-}
-
-type InTransaction func(tm TransactionManager) (interface{}, error)
