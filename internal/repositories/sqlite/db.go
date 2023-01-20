93dfb18d1e79a84e5837a43c7913bc70a6db05c6
AkaCyberRat <andrejchuk01@gmail.com>
2023-01-20 21:37:06 +0300

    Refactor to clean architecture

--- internal/repositories/sqlite/db.go
+++ internal/repositories/sqlite/db.go
@@ -1,19 +1,24 @@
 package sqlite
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
-	"github.com/sirupsen/logrus"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func NewSqliteConnection(filePath string) gorm.Dialector {
+func NewSqliteDb(filePath string) (*gorm.DB, error) {
 	folderPath, _ := filepath.Split(filePath)
 	if err := os.MkdirAll(folderPath, 0777); err != nil {
-		logrus.Fatal("Failed to create db folder:", err.Error())
+		return nil, fmt.Errorf("failed to create db folder: %v", err.Error())
 	}
 
-	return sqlite.Open(filePath)
+	db, err := gorm.Open(sqlite.Open(filePath), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("db error: %v", err.Error())
+	}
+
+	return db, nil
 }
