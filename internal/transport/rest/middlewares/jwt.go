e418410616773cb413f3a5dcdf2ad6007774cf17
AkaCyberRat <andrejchuk01@gmail.com>
2023-01-24 19:40:09 +0300

    Change error response methods

--- internal/transport/rest/middlewares/jwt.go
+++ internal/transport/rest/middlewares/jwt.go
@@ -1,9 +1,12 @@
 package middlewares
 
 import (
-	"WireguardManager/internal/tools/auth"
+	"errors"
 	"net/http"
 	"strings"
+
+	"WireguardManager/internal/tools/auth"
+	"WireguardManager/internal/transport/rest"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,31 +19,32 @@ func NewJwtHandler(authTool auth.AuthTool) *JwtHandler {
 	return &JwtHandler{authTool: authTool}
 }
 
-func (h *JwtHandler) Authenticate() func(c *gin.Context) {
+func (h *JwtHandler) Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		if !h.authTool.IsEnabled() {
 			c.Next()
+
 			return
 		}
 
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "request does not contain an access token"})
+			rest.NewErrorResponse(c, http.StatusUnauthorized, ErrNoAccessToken)
 
 			return
 		}
 
 		headerParts := strings.Split(authHeader, " ")
 		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "request contains invalid authorization header format"})
+			rest.NewErrorResponse(c, http.StatusUnauthorized, ErrInvalidAuthHeaderFormat)
 
 			return
 		}
 
 		claims, err := h.authTool.ValidateToken(headerParts[1])
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			rest.NewErrorResponse(c, http.StatusUnauthorized, err)
 
 			return
 		}
@@ -51,18 +55,19 @@ func (h *JwtHandler) Authenticate() func(c *gin.Context) {
 	}
 }
 
-func (h *JwtHandler) AllowedRoles(roles ...string) func(c *gin.Context) {
+func (h *JwtHandler) AllowedRoles(roles ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		if !h.authTool.IsEnabled() {
 			c.Next()
+
 			return
 		}
 
 		claims := c.Keys["Claims"].(auth.JwtClaims)
 
 		if !h.authTool.ValidateRoles(roles, claims.Roles) {
-			c.AbortWithStatusJSON(http.StatusForbidden, nil)
+			rest.NewErrorResponse(c, http.StatusForbidden, ErrNotEnoughRights)
 
 			return
 		}
@@ -70,3 +75,9 @@ func (h *JwtHandler) AllowedRoles(roles ...string) func(c *gin.Context) {
 		c.Next()
 	}
 }
+
+var (
+	ErrNoAccessToken           = errors.New("request does not contain an access token")
+	ErrInvalidAuthHeaderFormat = errors.New("request contains invalid authorization header format")
+	ErrNotEnoughRights         = errors.New("not enough rights to access")
+)
